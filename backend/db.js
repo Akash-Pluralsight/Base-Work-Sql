05fbde09ad8f5de46faa26c92eac43a0aa756233
Akash <akashs-chetiar@pluralsight.com>
2024-02-09 10:50:24 +0530

    added .env

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'akashs-chetiar',
+    user: process.env.POSTGRES_USER,
     host: 'postgres',
-    database: 'akashs-chetiar',
-    password: '@Spvaaki88',
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
     port: 5432,
 });
 
